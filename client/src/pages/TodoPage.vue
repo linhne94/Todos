f86a269c27a2f73b92f09d473bff4cb4d9f7c4d0
yunben <vietzz771@gmail.com>
2024-07-17 17:58:40 +0700

    Init FE views

---
+++ client/src/pages/TodoPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import SearchBar from '@components/SearchBar.vue';
+  import TaskList from '@components/tasklist/TaskList.vue';
+
+  import { ref } from 'vue';
+
+  const tasks = ref([
+    { category: 'daily habits', title: 'Categories', checked: false },
+    { category: 'personal projects', title: 'Write for at least 30 minutes', checked: false },
+    { category: 'work', title: 'Exercise for 20 minutes', checked: false },
+    { category: 'daily habits', title: 'Read a book for 1 hour', checked: false },
+  ]);
+</script>
+
+<template>
+  <SearchBar :tasks="tasks" />
+  <TaskList :tasks="tasks" />
+</template>
