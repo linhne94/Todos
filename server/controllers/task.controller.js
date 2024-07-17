e5495fb6e2d2f29f300ab06edb549ff72582cca1
tqqv <trinhquang96vu@gmail.com>
2024-07-17 23:45:52 +0700

    add load all while no timerange

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -28,60 +28,64 @@ const getTasks = async (req, res, next) => {
     let startOfTomorrow;
     const { timeRange } = req.query;
     
-    switch (timeRange) {
-      case 'today':
-        startOfDay = moment().tz(timezone).startOf('day').add(7, 'hour').toDate();
-        startOfTomorrow = moment().tz(timezone).startOf('day').add(1, 'day').add(7, 'hour').toDate();
-
-        
-        tasks = await getAllTasks({
-          dueDate: {
-            [Op.gte]: startOfDay,
-            [Op.lte]: startOfTomorrow
-          }
-        });
-        break;
-        
-        
-        case 'tomorrow':
-          startOfDay = moment().tz(timezone).startOf('day').add(1, 'day').add(7, 'hour').toDate();;
-          startOfTomorrow = moment().tz(timezone).startOf('day').add(2, 'day').add(7, 'hour').toDate();
+    if(!timeRange) {
+      tasks = await getAllTasks()
+    } else {
+      switch (timeRange) {
+        case 'today':
+          startOfDay = moment().tz(timezone).startOf('day').add(7, 'hour').toDate();
+          startOfTomorrow = moment().tz(timezone).startOf('day').add(1, 'day').add(7, 'hour').toDate();
+  
           
-        tasks = await getAllTasks({
-          dueDate: {
-            [Op.gte]: startOfDay,
-            [Op.lte]: startOfTomorrow
-          }
-        });
-        break;
-      
-      case 'next_week':
-         startOfDay = moment().add(1, 'week').startOf('week').add(1, 'day');
-         startOfTomorrow = moment().add(1, 'week').endOf('week').add(1, 'day');
-        
-        tasks = await getAllTasks({
-          dueDate: {
-            [Op.gte]: startOfDay,
-            [Op.lte]: startOfTomorrow
-          }
-        });
-        break;
-      
-      case 'this_week':
-        startOfDay = moment().startOf('week').add(1, 'day');
-        startOfTomorrow = moment().endOf('week').add(1, 'day');
-        
-        tasks = await getAllTasks({
+          tasks = await getAllTasks({
             dueDate: {
               [Op.gte]: startOfDay,
               [Op.lte]: startOfTomorrow
             }
-        });
-        break;
-      default:
-        throw new Error('Invalid time range');
+          });
+          break;
+          
+          
+          case 'tomorrow':
+            startOfDay = moment().tz(timezone).startOf('day').add(1, 'day').add(7, 'hour').toDate();;
+            startOfTomorrow = moment().tz(timezone).startOf('day').add(2, 'day').add(7, 'hour').toDate();
+            
+          tasks = await getAllTasks({
+            dueDate: {
+              [Op.gte]: startOfDay,
+              [Op.lte]: startOfTomorrow
+            }
+          });
+          break;
+        
+        case 'next_week':
+           startOfDay = moment().add(1, 'week').startOf('week').add(1, 'day');
+           startOfTomorrow = moment().add(1, 'week').endOf('week').add(1, 'day');
+          
+          tasks = await getAllTasks({
+            dueDate: {
+              [Op.gte]: startOfDay,
+              [Op.lte]: startOfTomorrow
+            }
+          });
+          break;
+        
+        case 'this_week':
+          startOfDay = moment().startOf('week').add(1, 'day');
+          startOfTomorrow = moment().endOf('week').add(1, 'day');
+          
+          tasks = await getAllTasks({
+              dueDate: {
+                [Op.gte]: startOfDay,
+                [Op.lte]: startOfTomorrow
+              }
+          });
+          break;
+        default:
+          throw new Error('Invalid time range');
+      }
     }
-
+    
     if (!tasks || tasks.length === 0) {
       throw new NotFoundError('No tasks found for the specified time range');
     }
